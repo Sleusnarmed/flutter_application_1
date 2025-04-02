0626222ed97d29676b73ef1277df4b8e7b1ef607
Jose Salazar <jmsalazar.macg@hotmail.com>
2025-04-01 20:47:58 -0600

    TextFormField

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:flutter_application_1/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:flutter_application_1/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -43,7 +44,7 @@ class _ChatView extends StatelessWidget {
                 },
               ),
             ),
-            Text('What'),
+            const MessageFieldBox(),
           ],
         ),
       ),
